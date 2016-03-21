4dc7f85240e45e5ac900042fb31b9c9ac38db796
Lucas <>
2016-03-21 19:43:57 -0300

    versao 2.0 - 28/03/16

--- Sudoku/src/sudoku/Main.java
+++ Sudoku/src/sudoku/Main.java
@@ -14,25 +14,29 @@ public class Main {
     public static void main(String[] args) {
         sudoku = new Sudoku();
         preenche();
-        sudoku.print();
     }
 
     private static void preenche() {
+        sudoku.print();
         permanentes();
         if (!sudoku.isComplete()) {
+            sudoku.print();
+            System.out.println("\nBackTrack\n");
             backtrack();
         }
+        sudoku.print();
     }
 
     public static void permanentes() {
         boolean mudou = false;
-        int possivel = 0, subMatriz;
-        for (int l = 0; l < sudoku.size(); l++) {
-            for (int c = 0; c < sudoku.size(); c++) {
+        int possivel = 0, subMatriz, lastSubMatriz = 0;
+        for (int l = 0; l < 9; l++) {
+            for (int c = 0; c < 9; c++) {
+                subMatriz = getSubMatriz(l, c);
                 for (int n = 1; n < 10; n++) {
                     if (sudoku.get(l, c) == 0) {
                         if (!(sudoku.linhaContem(l, n) || sudoku.colunaContem(c, n))) {
-                            if (!sudoku.subMatrizContem(getSubMatriz(l, c), n)) {
+                            if (!sudoku.subMatrizContem(subMatriz, n)) {
                                 if (possivel == 0) {
                                     possivel = n;
                                 } else {
@@ -45,8 +49,12 @@ public class Main {
                 }
                 if (possivel != 0) {
                     mudou = true;
-                    sudoku.set(l, c, possivel);
+                    sudoku.setPermanente(l, c, possivel);
                     possivel = 0;
+                } else if (lastSubMatriz < subMatriz) {
+                    boolean mudouSM = permanentesSubMatriz(subMatriz);
+                    lastSubMatriz = subMatriz;
+                    mudou = mudouSM;
                 }
             }
         }
@@ -55,33 +63,97 @@ public class Main {
         }
     }
 
-    private static void backtrack() {
-        boolean achou = true;
-        int i = 1;
-        for (int l = 0; l < sudoku.size(); l++) {
-            for (int c = 0; c < sudoku.size(); c++) {
-                if (sudoku.get(l, c) == 0) {
-                    if (!achou && sudoku.hasBackup()) {
-                        int r[];
-                        do {
-                            r = sudoku.restoreBackup();
-                            i = r[2] + 1;
-                        } while (i > 9 && sudoku.hasBackup());
-                        l = r[0];
-                        c = r[1];
-                    } else {
-                        achou = false;
+    private static boolean permanentesSubMatriz(int subMatriz) {
+        int possivel = 0, col = 10, lin = 10, linhaInit, subMatrizAux = subMatriz;
+        if (subMatrizAux < 4) {
+            linhaInit = 0;
+        } else if (subMatrizAux < 7) {
+            linhaInit = 3;
+            subMatrizAux -= 3;
+        } else {
+            linhaInit = 6;
+            subMatrizAux -= 6;
+        }
+        int colInit = (subMatrizAux == 1) ? 0 : (subMatrizAux == 2) ? 3 : 6;
+        boolean mudou = false;
+        numero:
+        for (int n = 1; n < 10; n++) {
+            linha:
+            for (int l = linhaInit; l < linhaInit + 3; l++) {
+                for (int c = colInit; c < colInit + 3; c++) {
+                    if (sudoku.subMatrizContem(subMatriz, n)) {
+                        continue numero;
                     }
+                    if (sudoku.linhaContem(l, n)) {
+                        continue linha;
+                    }
+                    if (sudoku.colunaContem(c, n)) {
+                        continue;
+                    }
+                    possivel++;
+                    col = c;
+                    lin = l;
+                    if (possivel == 2) {
+                        col = 10;
+                        lin = 10;
+                        possivel = 0;
+                        continue numero;
+                    }
+                }
+            }
+            if (possivel == 1 && lin < 10 && col < 10 && sudoku.get(lin, col) == 0) {
+                mudou = true;
+                sudoku.setPermanente(lin, col, n);
+            }
+            possivel = 0;
+        }
+        return mudou;
+    }
+
+    private static void backtrack() {
+        boolean achou = false;
+        int i = 1;
+        for (int l = 0; l < 9; l++) {
+            for (int c = 0; c < 9; c++) {
+                if (sudoku.isNotOriginal(l, c)) {
                     for (int n = i; n < 10; n++) {
-                        if (!(sudoku.linhaContem(l, n) || sudoku.colunaContem(c, n) || sudoku.subMatrizContem(getSubMatriz(l, c), n))) {
-                            achou = true;
-                            sudoku.set(l, c, n);
-                            sudoku.print();
-                            System.out.println("");
-                            break;
+                        if (!(sudoku.linhaContem(l, n) || sudoku.colunaContem(c, n))) {
+                            if (!sudoku.subMatrizContem(getSubMatriz(l, c), n)) {
+                                achou = true;
+                                sudoku.set(l, c, n);
+                                sudoku.print();
+                                break;
+                            } else if (getSubMatriz(l, c) == 3 || getSubMatriz(l, c) == 6 || getSubMatriz(l, c) == 9) {
+                                achou = false;
+                                break;
+                            }
                         }
                     }
-                    i = 1;
+                    if (!achou) {
+                        do {
+                            if (c == 0) {
+                                c = 8;
+                                if (l != 0) {
+                                    l--;
+                                }
+                            } else {
+                                c--;
+                            }
+                        } while (!sudoku.isNotOriginal(l, c) && c >= 0 && l >= 0);
+                        i = sudoku.get(l, c) + 1;
+                        sudoku.set(l, c, 0);
+                        if (c == 0) {
+                            c = 8;
+                            if (l != 0) {
+                                l--;
+                            }
+                        } else {
+                            c--;
+                        }
+                    } else {
+                        i = 1;
+                        achou = false;
+                    }
                 }
             }
         }
--- Sudoku/src/sudoku/Sudoku.java
+++ Sudoku/src/sudoku/Sudoku.java
@@ -5,19 +5,27 @@
  */
 package sudoku;
 
-import java.util.Stack;
-
 /**
  *
  * @author 31471730
  */
 public class Sudoku {
 
-    private final int[][] sudoku;
-    private Stack<String> backup;
+    private final int[][] sudokuOrig;
+    private final int[][] sudokuResp;
+//    private Stack<String> backup;
 
     public Sudoku() {
-        int[][] sudoku = {
+        int[][] origSudoku = {
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0}};
             {8, 0, 0, 0, 0, 0, 0, 0, 0},
             {0, 0, 3, 6, 0, 0, 0, 0, 0},
             {0, 7, 0, 0, 9, 0, 2, 0, 0},
@@ -27,22 +35,51 @@ public class Sudoku {
             {0, 0, 1, 0, 0, 0, 0, 6, 8},
             {0, 0, 8, 5, 0, 0, 0, 1, 0},
             {0, 9, 0, 0, 0, 0, 4, 0, 0}};
-//        {5, 3, 0, 0, 7, 0, 0, 0, 0},
-//        {6, 0, 0, 1, 9, 5, 0, 0, 0},
-//        {0, 9, 8, 0, 0, 0, 0, 6, 0},
-//        {8, 0, 0, 0, 6, 0, 0, 0, 3},
-//        {4, 0, 0, 8, 0, 3, 0, 0, 1},
-//        {7, 0, 0, 0, 2, 0, 0, 0, 6},
-//        {0, 6, 0, 0, 0, 0, 2, 8, 0},
-//        {0, 0, 0, 4, 1, 9, 0, 0, 5},
-//        {0, 0, 0, 0, 8, 0, 0, 7, 9},};
-        this.sudoku = sudoku;
-        backup = new Stack<>();
+//            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+//            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+//            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+//            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+//            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+//            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+//            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+//            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+//            {0, 0, 0, 0, 8, 0, 0, 7, 9},};
+        int[][] respSudoku = {
+            //            {5, 3, 0, 0, 7, 0, 0, 0, 0},
+            //            {6, 0, 0, 1, 9, 5, 0, 0, 0},
+            //            {0, 9, 8, 0, 0, 0, 0, 6, 0},
+            //            {8, 0, 0, 0, 6, 0, 0, 0, 3},
+            //            {4, 0, 0, 8, 0, 3, 0, 0, 1},
+            //            {7, 0, 0, 0, 2, 0, 0, 0, 6},
+            //            {0, 6, 0, 0, 0, 0, 2, 8, 0},
+            //            {0, 0, 0, 4, 1, 9, 0, 0, 5},
+            //            {0, 0, 0, 0, 8, 0, 0, 7, 9},};
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0},
+            //            {0, 0, 0, 0, 0, 0, 0, 0, 0}};
+            {8, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 3, 6, 0, 0, 0, 0, 0},
+            {0, 7, 0, 0, 9, 0, 2, 0, 0},
+            {0, 5, 0, 0, 0, 7, 0, 0, 0},
+            {0, 0, 0, 0, 4, 5, 7, 0, 0},
+            {0, 0, 0, 1, 0, 0, 0, 3, 0},
+            {0, 0, 1, 0, 0, 0, 0, 6, 8},
+            {0, 0, 8, 5, 0, 0, 0, 1, 0},
+            {0, 9, 0, 0, 0, 0, 4, 0, 0}};
+        this.sudokuOrig = origSudoku;
+        this.sudokuResp = respSudoku;
+//        backup = new Stack<>();
     }
 
     public boolean linhaContem(int l, int n) {
-        for (int i = 0; i < sudoku.length; i++) {
-            if (sudoku[l][i] == n) {
+        for (int i = 0; i < sudokuResp.length; i++) {
+            if (sudokuResp[l][i] == n) {
                 return true;
             }
         }
@@ -50,8 +87,8 @@ public class Sudoku {
     }
 
     public boolean colunaContem(int c, int n) {
-        for (int i = 0; i < sudoku.length; i++) {
-            if (sudoku[i][c] == n) {
+        for (int i = 0; i < sudokuResp.length; i++) {
+            if (sudokuResp[i][c] == n) {
                 return true;
             }
         }
@@ -72,7 +109,7 @@ public class Sudoku {
         int colInit = (subMatriz == 1) ? 0 : (subMatriz == 2) ? 3 : 6;
         for (int l = linhaInit; l < linhaInit + 3; l++) {
             for (int c = colInit; c < colInit + 3; c++) {
-                if (sudoku[l][c] == n) {
+                if (sudokuResp[l][c] == n) {
                     return true;
                 }
             }
@@ -81,31 +118,41 @@ public class Sudoku {
     }
 
     public void print() {
-        for (int l = 0; l < sudoku.length; l++) {
-            for (int c = 0; c < sudoku[l].length; c++) {
-                System.out.print(sudoku[l][c] + " ");
+        for (int l = 0; l < sudokuResp.length; l++) {
+            for (int c = 0; c < sudokuResp[l].length; c++) {
+                System.out.print(sudokuResp[l][c] + " ");
             }
             System.out.println("");
         }
+        System.out.println("");
     }
 
     public int size() {
-        return sudoku.length;
+        return sudokuResp.length;
     }
 
     public int get(int l, int c) {
-        return sudoku[l][c];
+        return sudokuResp[l][c];
     }
 
     public void set(int l, int c, int n) {
-        backup.push(l + "," + c + "," + n);
-        sudoku[l][c] = n;
+//        backup.push(l + "," + c + "," + n);
+        if (sudokuOrig[l][c] == 0)
+            sudokuResp[l][c] = n;
+    }
+
+    public void setPermanente(int l, int c, int n) {
+//        backup.push(l + "," + c + "," + n);
+        if (sudokuOrig[l][c] == 0) {
+            sudokuOrig[l][c] = n;
+            sudokuResp[l][c] = n;
+        }
     }
 
     public boolean isComplete() {
-        for (int l = 0; l < sudoku.length; l++) {
-            for (int c = 0; c < sudoku[l].length; c++) {
-                if (sudoku[l][c] == 0) {
+        for (int l = 0; l < sudokuResp.length; l++) {
+            for (int c = 0; c < sudokuResp[l].length; c++) {
+                if (sudokuResp[l][c] == 0) {
                     return false;
                 }
             }
@@ -113,16 +160,20 @@ public class Sudoku {
         return true;
     }
 
-    public int[] restoreBackup() {
-        String lastMove = backup.pop();
-        String[] pos = lastMove.split(",");
-        int l = Integer.parseInt(pos[0]), c = Integer.parseInt(pos[1]), n = Integer.parseInt(pos[2]);
-        sudoku[l][c] = 0;
-        int r[] = {l, c, n};
-        return r;
+    public boolean isNotOriginal(int l, int c) {
+        return sudokuOrig[l][c] == 0;
     }
 
-    public boolean hasBackup() {
-        return !backup.empty();
-    }
+//    public int[] restoreBackup() {
+//        String lastMove = backup.pop();
+//        String[] pos = lastMove.split(",");
+//        int l = Integer.parseInt(pos[0]), c = Integer.parseInt(pos[1]), n = Integer.parseInt(pos[2]);
+//        sudokuResp[l][c] = 0;
+//        int r[] = {l, c, n};
+//        return r;
+//    }
+//
+//    public boolean hasBackup() {
+//        return !backup.empty();
+//    }
 }
